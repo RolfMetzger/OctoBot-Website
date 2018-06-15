97e443b445210e4e9d51f224c16abd69dde1b34a
Laurent BOUQUET <laurent.bouquet@joliciel.fr>
2018-06-15 10:17:28 +0200

    Ordering users by username in index

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
- * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class UserRepository extends ServiceEntityRepository
@@ -17,6 +16,13 @@ class UserRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    public function findAll()
+    {
+        $builder = $this->createQueryBuilder('u');
+        $builder->orderBy('u.username', 'ASC');
+        return $builder->getQuery()->getResult();
     }
 
 //    /**
